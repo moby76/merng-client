ead8a9a78049151c1e898830335a4d54d4091ce3
Pavel Alyamovski <moby76@mail.ru>
2021-06-09 00:57:19 +0700

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,7 +6,7 @@ import { setContext } from "apollo-link-context";
 //1.
 //создать константу - подключение к graphQL
 const httpLink = createHttpLink({
-   uri: 'http://localhost:5000'
+   uri: 'https://floating-spire-77624.herokuapp.com/'
  });
 
 //2.
