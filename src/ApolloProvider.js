17073db4a517c9de180363e2acf454db8aca0446
Pavel Alyamovski <moby76@mail.ru>
2021-09-07 01:48:33 +0700

    @apollo/client/link/context

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,7 +1,7 @@
 //1 Нужно обернуть приложение в Аполло-провайдер
 import App from './App'
 import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, createHttpLink } from '@apollo/client'
-import { setContext } from "apollo-link-context";
+import { setContext } from "@apollo/client/link/context";
 
 //1.
 //создать константу - подключение к graphQL
@@ -12,19 +12,21 @@ const httpLink = createHttpLink({
 })
 
 //2.
-//Включаем заголовок авторизации в запрос
+//Для самоидентификации при использовании HTTP Включаем заголовок авторизации в запрос
 //Функция setContext принимает функцию, которая возвращает либо объект, либо обещание, которое затем возвращает объект для установки нового контекста запроса.
 //https://www.apollographql.com/docs/react/api/link/apollo-link-context/
-const authLink = setContext(() => {
-   //получим токен из локального хранилища
+const authLink = setContext((_, { headers }) => {
+   //получить токен аутентификации из локального хранилища, если он существует
    const token = localStorage.getItem('jwtToken')
    //вернуть модифицированный запрос 
-   //получить хеадеры
+   //вернуть заголовки в контекст, чтобы httpLink мог их прочитать
    return {
       headers: {
+         ...headers,
          //если токен получен то подставить его в строку хеадера отвечающую за авторизацию через токен, иначе передать пустую строку на сервер
          Authorization: token ? `Bearer ${token}` : '',
-         // "Access-Control-Allow-Origin": "https://floating-spire-77624.herokuapp.com/"
+         //Сервер может использовать этот заголовок для аутентификации пользователя и присоединения его к контексту выполнения GraphQL, чтобы 
+         //преобразователи могли изменять свое поведение в зависимости от роли и разрешений пользователя.
       }
    }
 })
